f4c4a460622212b9559c150e35c73cc6d47e6997
KariFra <mystery.tomorrow.2@gmail.com>
2019-11-09 11:09:31 +0100

    First Commit

--- kodilla-testing/src/main/java/com/kodilla/testing/Calculator/SimpleCalculator.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/Calculator/SimpleCalculator.java
@@ -2,20 +2,6 @@ package com.kodilla.testing.Calculator;
 
 public class SimpleCalculator {
 
-    int a;
-    int b;
-
-    public SimpleCalculator(){
-        this.a = a;
-        this.b = b;
-    }
-    public int getA(){
-        return a;
-    }
-    public int getB(){
-        return b;
-    }
-
     public int add (int a, int b){
         return a + b;
     }
