efef042f76617161a63a326044b44fbde68be66c
KariFra <mystery.tomorrow.2@gmail.com>
2019-11-20 10:07:43 +0100

    after using getters

--- kodilla-testing/src/main/java/com/kodilla/testing/forum/statistics/UserSpecification.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/forum/statistics/UserSpecification.java
@@ -47,8 +47,7 @@ public class UserSpecification{
         return avCommentPerPost;
     }
 
-    public String calculateAdvStatistics(Statistics statistics) {
-        String result = "";
+    public void calculateAdvStatistics(Statistics statistics) {
         userNumber = statistics.usersNames().size();
         postNumber = statistics.postsCount();
         commentNumber = statistics.commentsCount();
@@ -56,26 +55,20 @@ public class UserSpecification{
             avPostPerUser = postNumber/userNumber;
             avCommentPerUser = commentNumber/userNumber;
             avCommentPerPost = postNumber/commentNumber;
-            result = userNumber+", "+postNumber+", " +commentNumber+", " +avPostPerUser+", " +avCommentPerUser+", "+avCommentPerPost;
             System.out.println(userNumber+", "+postNumber+", " +commentNumber+", " +avPostPerUser+", " +avCommentPerUser+", "+avCommentPerPost);}
         if (userNumber == 0){
-            result = "There are no users.";
             System.out.println("There are no users.");
         }
         if(commentNumber == 0 && postNumber != 0 && userNumber != 0){
             avPostPerUser = postNumber/userNumber;
-            result = getUserNumber()+", "+ getPostNumber()+", "+ getAvPostPerUser();
             System.out.println("There are "+userNumber+" users and "+postNumber+" posts but there are no comments. The avarage post per user equals:"+avPostPerUser);
         }
         if(postNumber == 0 && commentNumber != 0 && userNumber != 0){
             avCommentPerUser = commentNumber/userNumber;
-            result = userNumber+", "+ commentNumber+", "+avCommentPerUser;
             System.out.println( "There are "+userNumber+" users and "+commentNumber+" comments but there are no posts. The avarage comment per user equals: "+avCommentPerUser);
         }
         if(postNumber == 0 && commentNumber == 0 && userNumber != 0){
-            result =""+userNumber;
             System.out.println("There are "+userNumber+" users and no comments or posts.");
         }
-        return result;
     }
 }
--- kodilla-testing/src/test/java/com/kodilla/testing/forum/statistics/ForumStatisticsTestSuite.java
+++ kodilla-testing/src/test/java/com/kodilla/testing/forum/statistics/ForumStatisticsTestSuite.java
@@ -20,7 +20,7 @@ public class ForumStatisticsTestSuite {
     }
 
     @Test
-   public void calculateAdvStatisticsZeroCommentsandHundredUsers(){
+   public void calculateAdvStatisticsZeroCommentsAndHundredUsers(){
         //Given
         Statistics statisticsMock = mock(Statistics.class);
         List<String> usersNames = userGenerator(99);
@@ -37,6 +37,10 @@ public class ForumStatisticsTestSuite {
 
         //Than
         Assert.assertEquals(userSpecification.getUserNumber(), 100);
+        Assert.assertEquals(userSpecification.getCommentNumber(), 0);
+        Assert.assertEquals(userSpecification.getPostNumber(), 1000);
+        Assert.assertEquals(userSpecification.getAvPostPerUser(), 10.0, 0.01);
+        Assert.assertEquals(userSpecification.getAvCommentPerUser(), 0.0, 0.01 );
     }
     @Test
     public void calculateAdvStatisticsNoUser(){
@@ -52,10 +56,10 @@ public class ForumStatisticsTestSuite {
         UserSpecification userSpecification = new UserSpecification(statisticsMock);
 
         //When
-        String result = userSpecification.calculateAdvStatistics(statisticsMock);
+        userSpecification.calculateAdvStatistics(statisticsMock);
 
         //Than
-        Assert.assertEquals("There are no users.",result);
+        Assert.assertEquals(userSpecification.getUserNumber(), 0);
     }
     @Test
     public void calculateAdvStatisticsZeroPosts(){
@@ -71,10 +75,11 @@ public class ForumStatisticsTestSuite {
         UserSpecification userSpecification = new UserSpecification(statisticsMock);
 
         //When
-        String result = userSpecification.calculateAdvStatistics(statisticsMock);
+        userSpecification.calculateAdvStatistics(statisticsMock);
 
         //Than
-        Assert.assertEquals("5, 5, 1.0",result);
+        Assert.assertEquals(userSpecification.getPostNumber(), 0);
+
     }
     @Test
     public void calculateAdvStatisticsMorePosts(){
@@ -90,18 +95,18 @@ public class ForumStatisticsTestSuite {
         UserSpecification userSpecification = new UserSpecification(statisticsMock);
 
         //When
-        String result = userSpecification.calculateAdvStatistics(statisticsMock);
+        userSpecification.calculateAdvStatistics(statisticsMock);
 
         //Than
-        Assert.assertEquals("10, 10, 5, 1.0, 0.0, 2.0",result);
+        Assert.assertEquals(userSpecification.getAvCommentPerPost(), 2.0, 0.1);
     }
     @Test
     public void calculateAdvStatisticsMoreComments(){
         //Given
         Statistics statisticsMock = mock(Statistics.class);
-        List<String> usersNames = userGenerator(9);
-        int postNumber = 5;
-        int commentNumber = 10;
+        List<String> usersNames = userGenerator(19);
+        int postNumber = 20;
+        int commentNumber = 40;
         when(statisticsMock.usersNames()).thenReturn(usersNames);
         when(statisticsMock.postsCount()).thenReturn(postNumber);
         when(statisticsMock.commentsCount()).thenReturn(commentNumber);
@@ -109,9 +114,13 @@ public class ForumStatisticsTestSuite {
         UserSpecification userSpecification = new UserSpecification(statisticsMock);
 
         //When
-        String result = userSpecification.calculateAdvStatistics(statisticsMock);
+        userSpecification.calculateAdvStatistics(statisticsMock);
 
         //Than
-        Assert.assertEquals("10, 5, 10, 0.0, 1.0, 0.0",result);
+        Assert.assertEquals(userSpecification.getUserNumber(), 20);
+        Assert.assertEquals(userSpecification.getCommentNumber(), 40);
+        Assert.assertEquals(userSpecification.getPostNumber(), 20);
+        Assert.assertEquals(userSpecification.getAvPostPerUser(), 1.0, 0.01);
+        Assert.assertEquals(userSpecification.getAvCommentPerUser(), 2.0, 0.01 );
     }
 }
