b0bd4d7898054fa591ba1d456d3ec058855eedcf
KariFra <mystery.tomorrow.2@gmail.com>
2019-12-03 13:47:34 +0100

    try-catch-finally

--- kodilla-exception/src/main/java/com/kodilla/exception/io/FileReader.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/io/FileReader.java
@@ -1,6 +1,5 @@
 package com.kodilla.exception.io;
 
-import jdk.internal.org.objectweb.asm.ClassReader;
 
 import java.io.File;
 import java.io.IOException;
@@ -10,19 +9,15 @@ import java.nio.file.Paths;
 import java.util.stream.Stream;
 
 public class FileReader {
-    public void readFile(){
-        ClassLoader classReader = getClass().getClassLoader();
-        File file = new File(ClassLoader.getSystemResource("file/names.txt").getFile());
+    public void readFile() {
+        ClassLoader classLoader = getClass().getClassLoader();
+        File file = new File(classLoader.getResource("File/names.txt").getFile());
         Path path = Paths.get(file.getPath());
-        System.out.println(file.getPath());
 
         try {
             Stream<String> fileLines = Files.lines(path);
-            fileLines.forEach(System.out::println);
-        } catch(IOException e ){
-        System.out.println("Ups something went wrong!"+ e);
+        }catch (IOException e) {
+            System.out.println("Oh no something went wrong!");
         }
-
     }
-
 }
--- kodilla-exception/src/main/java/com/kodilla/exception/main/ExceptionModuleRunner.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/main/ExceptionModuleRunner.java
@@ -2,8 +2,6 @@ package com.kodilla.exception.main;
 
 import com.kodilla.exception.io.FileReader;
 
-import java.nio.file.Path;
-import java.nio.file.Paths;
 
 public class ExceptionModuleRunner {
     public static void main (String []args){
