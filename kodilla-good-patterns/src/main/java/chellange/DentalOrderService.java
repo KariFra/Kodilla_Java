60dda0633f6fbdc4672a0629d6d7b77b7e288c98
KariFra <mystery.tomorrow.2@gmail.com>
2020-01-14 21:15:07 +0100

    Allegro

--- kodilla-good-patterns/src/main/java/chellange/DentalOrderService.java
+++ kodilla-good-patterns/src/main/java/chellange/DentalOrderService.java
@@ -5,6 +5,6 @@ import java.time.LocalDateTime;
 public class DentalOrderService implements OrderService {
     @Override
     public boolean sell(User user, Purchase purchase, LocalDateTime when) {
-        return false;
+        return true;
     }
 }
