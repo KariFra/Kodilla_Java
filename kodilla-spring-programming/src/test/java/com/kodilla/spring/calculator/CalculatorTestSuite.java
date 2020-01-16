4a8ac6fbc5d269e6216b171a0029f5788b9d62ab
KariFra <mystery.tomorrow.2@gmail.com>
2020-01-16 10:05:36 +0100

    calculator with return

--- kodilla-spring-programming/src/test/java/com/kodilla/spring/calculator/CalculatorTestSuite.java
+++ kodilla-spring-programming/src/test/java/com/kodilla/spring/calculator/CalculatorTestSuite.java
@@ -1,13 +1,13 @@
 package com.kodilla.spring.calculator;
 
 import com.kodilla.spring.library.Library;
+import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.util.Assert;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -20,12 +20,15 @@ public class CalculatorTestSuite {
         Calculator calculator = context.getBean(Calculator.class);
 
         //When
-        calculator.add(2,3);
-        calculator.sub(4,3);
-        calculator.mul(2,3);
-        calculator.div(6,3);
+        double addResult = calculator.add(2,3);
+        double subResult = calculator.sub(4,3);
+        double mulResult = calculator.mul(2,3);
+        double divResult = calculator.div(6,3);
 
         //Then
-        //do nothing
+        Assert.assertEquals(5.00,addResult,0.01);
+        Assert.assertEquals(1.00,subResult,0.01);
+        Assert.assertEquals(6.00,mulResult,0.01);
+        Assert.assertEquals(2.00,divResult,0.01);
     }
 }
