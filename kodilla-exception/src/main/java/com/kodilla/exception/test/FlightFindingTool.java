b4eedf70fc876559db2a5b41f2cacadf44bf7e22
KariFra <mystery.tomorrow.2@gmail.com>
2020-01-21 17:55:22 +0100

    exception

--- kodilla-exception/src/main/java/com/kodilla/exception/test/FlightFindingTool.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/FlightFindingTool.java
@@ -1,14 +1,19 @@
 package com.kodilla.exception.test;
 
+import java.util.HashMap;
+
 public class FlightFindingTool {
 
 
-    public void findFilght(Flight flight) throws RouteNotFoundException{
-        if(flight != null) {
-            System.out.println("The flight departs from " + flight.departureAirport + " and arrives to"+ flight.arrivalAirport);
-        } else {
-            throw new RouteNotFoundException("There is no flight with given departure and arrival sites");
-        }
+    public void findFilght(Flight flight) throws RouteNotFoundException {
+        HashMap<String, Boolean> flights = new HashMap<>();
+        if (flights.size() != 0) {
+            if (flights.containsKey(flight.arrivalAirport) && flights.containsKey(flight.departureAirport)) {
+                System.out.println("The flight departs from " + flight.departureAirport + " and arrives to" + flight.arrivalAirport);
+            } else {
+                throw new RouteNotFoundException("There is no flight with given departure and arrival sites");
+            }
 
+        }
     }
 }
