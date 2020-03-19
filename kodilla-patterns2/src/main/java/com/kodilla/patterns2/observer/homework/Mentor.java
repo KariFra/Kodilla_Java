d2934d03c8a510b5f09c585271dcc176bba7b89c
KariFra <mystery.tomorrow.2@gmail.com>
2020-03-19 17:04:31 +0100

    Observer

--- kodilla-patterns2/src/main/java/com/kodilla/patterns2/observer/homework/Mentor.java
+++ kodilla-patterns2/src/main/java/com/kodilla/patterns2/observer/homework/Mentor.java
@@ -1,22 +1,26 @@
 package com.kodilla.patterns2.observer.homework;
 
 public class Mentor implements Notifier {
-    private final String studentName;
+    private final String mentorName;
     private int exerciseCount;
 
-    public Mentor(String studentName) {
-        this.studentName = studentName;
+    public Mentor(String mentorName) {
+        this.mentorName = mentorName;
     }
 
     @Override
     public void notify(ExcercisesToCheck excercisesToCheck) {
-        System.out.println( studentName+ "send new exercise in topic: "+ excercisesToCheck.getModule() +"\n" +
-                              "in total: " + excercisesToCheck.getMessages().size() + " exercise/s.");
+        String exerciseFill = " exercise.";
+        if(excercisesToCheck.getMessages().size()>1){
+            exerciseFill = " exercises.";
+        }
+        System.out.println( mentorName+ " has to check new exercise in topic: "+ excercisesToCheck.getModule() +"\n" +
+                              "in total: " + excercisesToCheck.getMessages().size() + exerciseFill);
         exerciseCount++;
     }
 
-    public String getStudentName() {
-        return studentName;
+    public String getMentorName() {
+        return mentorName;
     }
 
     public int getExerciseCount() {
--- kodilla-patterns2/src/test/java/com/kodilla/patterns2/observer/homework/MentorTestSuite.java
+++ kodilla-patterns2/src/test/java/com/kodilla/patterns2/observer/homework/MentorTestSuite.java
@@ -9,21 +9,21 @@ public class MentorTestSuite {
         //Given
         ExcercisesToCheck aspect = new AspectOrientedProgrammingExercises();
         ExcercisesToCheck function = new FunctionalProgrammingExcercises();
-        Mentor student1 = new Mentor("Benny Hill");
-        Mentor student2 = new Mentor("John Cena");
-        Mentor student3 = new Mentor("Dr. Sleep");
-        aspect.assignStudent(student1);
-        aspect.assignStudent(student2);
-        function.assignStudent(student3);
-        function.assignStudent(student2);
+        Mentor mentor1 = new Mentor("Benny Hill");
+        Mentor mentor2 = new Mentor("John Cena");
+        Mentor mentor3 = new Mentor("Dr. Sleep");
+        aspect.assignStudent(mentor1);
+        aspect.assignStudent(mentor2);
+        function.assignStudent(mentor3);
+        function.assignStudent(mentor2);
         //When
         aspect.makeAndSendExercise("Learn how to use @Transactional in Hibernate");
         function.makeAndSendExercise("Your first Stream");
         function.makeAndSendExercise("Use Optional in your collection");
         //Than
-        assertEquals(1,student1.getExerciseCount());
-        assertEquals(3,student2.getExerciseCount());
-        assertEquals(2,student3.getExerciseCount());
+        assertEquals(1,mentor1.getExerciseCount());
+        assertEquals(3,mentor2.getExerciseCount());
+        assertEquals(2,mentor3.getExerciseCount());
     }
 
 
